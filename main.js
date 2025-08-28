91b921a29c8c24a7be549bf8810b164d3b2fdcaa
mariko85 <m.chaxvashvili@gmail.com>
2025-08-28 18:10:19 +0400

    add

---
+++ main.js
@@ -0,0 +1,50 @@
+// Write a function to split a string and convert it into an array of words.
+// let str= "I love arrays they are my favorite"
+// function stringSplit(str){
+//     return str.split(" ")
+
+// }
+// let result=stringSplit(str)
+// console.log(result)
+
+
+
+
+//Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+// ["apple","rottenBanana","apple"]  ["apple","banana","apple"]
+
+// let array =["apple","rottenBanana","apple"]
+
+// const array1=array.map(item => {
+
+// if(item.includes("rotten")){
+//  return item=item.slice(6).toLowerCase()
+   
+// } else{
+//      return item=item
+// }
+ 
+
+// })
+// console.log(array1)
+
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+// let array =[5,5,7,"m", ]
+// let sum=0;
+// let array1=array.forEach(item => {
+
+//     if(typeof item == "number" ){
+//         sum +=item
+//     }
+
+// })
+// console.log(sum)
+
+function(a,b,c,d){
+
+}
+
